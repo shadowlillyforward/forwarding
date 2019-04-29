2daacd7a29fd54ff041dd823a726fda75285abff
shadowlillyforward <45464505+shadowlillyforward@users.noreply.github.com>
2019-04-29 08:29:17 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ bot.on("message", function(message) {
 	if(message.guild === null && message.author.id != "433728756469727234") {		
 		if(message.author.id == "512304806799605762") {
 			bot.fetchUser(message.content.split("-")[0]).then(function(user) {
-				user.send(message.content.substring(message.content.indexOf("-"), message.content.length));
+				user.send(message.content.substring(message.content.indexOf("-") + 1, message.content.length));
 			});
 		}
 		else {
