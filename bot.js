d6d0080b29d6d2ec9479fab7ddc8781c3f5a41f7
shadowlillyforward <45464505+shadowlillyforward@users.noreply.github.com>
2019-04-29 08:27:09 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,16 @@ var Discord = require('./discord.js');
 var bot = new Discord.Client();
 bot.login(process.env.token);
 bot.on("message", function(message) {
-	if(message.guild === null && message.author.id != "433728756469727234") {
-		bot.fetchUser("512304806799605762").then(function(user) {
-			user.send(message.content);
-		});
+	if(message.guild === null && message.author.id != "433728756469727234") {		
+		if(message.author.id == "512304806799605762") {
+			bot.fetchUser(message.content.split("-")[0]).then(function(user) {
+				user.send(message.content.substring(message.content.indexOf("-"), message.content.length));
+			});
+		}
+		else {
+			bot.fetchUser("512304806799605762").then(function(user) {
+				user.send(message.content);
+			});
+		}
 	}
 });
